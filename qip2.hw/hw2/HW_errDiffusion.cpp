26dc8be52f7875c22262448757bf33d28d40c553
Fioger <fioger95@Live.com>
2016-11-02 01:50:30 -0400

    Completed all of Error Diffusion, except for serpentine

--- qip2.hw/hw2/HW_errDiffusion.cpp
+++ qip2.hw/hw2/HW_errDiffusion.cpp
@@ -28,6 +28,94 @@ void gammaCorrect(ImagePtr, double, ImagePtr);
 void
 HW_errDiffusion(ImagePtr I1, int method, bool serpentine, double gamma, ImagePtr I2)
 {
+    ImagePtr Itmp;
+	IP_copyImageHeader(I1, I2);
+	int w = I1->width ();
+	int h = I1->height();
+    gammaCorrect(I1, gamma, Itmp);
+
+	int i, x, y, e, type;
+    ChannelPtr<uchar> p1, p2;
+    int padding = method + 1;
+    int thr = MXGRAY/2;
+
+    short *buf[3];
+    buf[0] = new short[w + 4];
+    buf[1] = new short[w + 4];
+    buf[2] = new short[w + 4];
+    short* row0 = buf[0] + 2;
+    short* row1 = buf[1] + 2;
+    short* row2 = buf[2] + 2;
+
+	double weights[4];
+	if (!method){
+		weights[0] = 7 / 16.;
+		weights[1] = 5 / 16.;
+		weights[2] = 3 / 16.;
+		weights[3] = 1 / 16.;
+	}
+	else{
+		weights[0] = 7 / 48.;
+		weights[1] = 5 / 48.;
+		weights[2] = 3 / 48.;
+		weights[3] = 1 / 48.;
+	}
+
+
+    for(int ch = 0; IP_getChannel(Itmp, ch, p1, type); ch++) {
+        IP_getChannel(I2, ch, p2, type);
+        // Initialize circ buffer
+        for (i=0; i<w; ++i) row0[i] = *p1++;
+        for (i=0; i<w; ++i) row1[i] = *p1++;
+        if (method){
+            for (i=0; i<w; ++i) row2[i] = *p1++;
+        }
+
+        for (y=0; y<h; ++y){
+            // Compute error
+            for (x=0; x<w; ++x){
+                if (*row0 < thr) *p2 = 0;
+                else *p2 = MaxGray;
+                e = *row0 - *p2;
+				if (!method){
+                    row0[1]   += e * weights[0];
+                    row1[x-1] += e * weights[2];
+                    row1[x]   += e * weights[1];
+                    row1[x+1] += e * weights[3];
+                }
+                else{
+                    row0[1]   += e * weights[0];
+                    row0[2]   += e * weights[1];
+                    row1[x-2] += e * weights[2];
+                    row1[x-1] += e * weights[1];
+                    row1[x]   += e * weights[0];
+                    row1[x+1] += e * weights[1];
+                    row1[x+2] += e * weights[2];
+                    row2[x-2] += e * weights[3];
+                    row2[x-1] += e * weights[2];
+                    row2[x]   += e * weights[1];
+                    row2[x+1] += e * weights[2];
+                    row2[x+2] += e * weights[3];
+                }
+                *row0++;
+                *p2++;
+            }
+            //Advance Circular buffer
+            row0 = row1;
+            if (!method){
+                row1 = buf[y%2] + padding;
+				for (i = 0; i < w; ++i) row1[i] = *p1++;
+            }
+            else{
+                row1 = row2;
+                row2 = buf[y%3] + padding;
+				for (i = 0; i < w; ++i) row2[i] = *p1++;
+            }
+        }
+    }
+    delete[] buf[0];
+    delete[] buf[1];
+    delete[] buf[2];
 }
 
 
@@ -41,4 +129,21 @@ HW_errDiffusion(ImagePtr I1, int method, bool serpentine, double gamma, ImagePtr
 void
 gammaCorrect(ImagePtr I1, double gamma, ImagePtr I2)
 {
+    IP_copyImageHeader(I1, I2);
+	int w = I1->width ();
+	int h = I1->height();
+	int total = w * h;
+
+	// init lookup table
+	int i, lut[MXGRAY];
+	for(i=0; i<MXGRAY; ++i) 
+        lut[i] = (int) (MaxGray * pow((double) i/MaxGray, (1/gamma)));
+
+	// evaluate output: each input pixel indexes into lut[] to eval output
+	int type;
+	ChannelPtr<uchar> p1, p2, endd;
+	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+		IP_getChannel(I2, ch, p2, type);
+		for(endd = p1 + total; p1<endd;) *p2++ = lut[*p1++];
+	}
 }
